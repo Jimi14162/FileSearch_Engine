005947ce0c757badce1c6760e1d237ba9a20c869
Jimi Patel <pjimi1746@gmail.com>
2026-05-16 14:23:13 +0530

    Added query parsing

--- include/Indexer.h
+++ include/Indexer.h
@@ -4,11 +4,14 @@
 #include<string>
 #include<vector>
 #include<unordered_map>
+#include<set>
 using namespace std;
 class Indexer
 {
+    set<string>stop;
     unordered_map<string,unordered_map<string,int>> m;
     public:
+    Indexer();
     void index_find(vector <string> & words,string &Filename);
     unordered_map <string,int> search(vector <string> &word);
     
--- src/Indexer.cpp
+++ src/Indexer.cpp
@@ -3,13 +3,40 @@
 #include<vector>
 #include<Indexer.h>
 #include<unordered_map>
+#include<set>
 using namespace std;
-
+Indexer :: Indexer()
+{
+    //Articles
+    stop.insert("a");
+    stop.insert("an");
+    stop.insert("the");
+    //Prepositions
+    stop.insert("in");
+    stop.insert("on");
+    stop.insert("at");
+    stop.insert("by");
+    stop.insert("from");
+    stop.insert("to");
+    //Helping Verbs
+    stop.insert("is");
+    stop.insert("are");
+    stop.insert("was");
+    stop.insert("were");
+    stop.insert("be");
+    //Conjuctions
+    stop.insert("and");
+    stop.insert("or");
+    stop.insert("but");
+    
+}
 void Indexer :: index_find(vector<string>&words,string &file_name)
 {
+    
     for(auto &w : words)
     {
-        m[w][file_name]++;
+        if(stop.find(w) == stop.end())
+            m[w][file_name]++;
     }
 }
 unordered_map <string,int> Indexer :: search(vector <string >&word)
